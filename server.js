0b6611d27a3253d9c7150cc15fd9559964bfaf0a
Josh Isaacson <jsisaacs@iu.edu>
2018-05-31 10:15:25 -0400

    Added request-promise and changed the requests in summoner-stats to use promises.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const request = require("request");
+const requestPromise = require('request-promise');
 const cors = require("cors");
 const queryString = require("query-string");
 const path = require("path");
@@ -25,43 +25,50 @@ app.get("/summoner-stats/:summonerName", (req, res) => {
     winrate: undefined,
     hotStreak: undefined,
   };
-  
+
   /*
     summoner/v3/summoners/by-name/{summonerName}
-  */
-
-  const summonerEndpointRequest = {
-    url: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.summonerName}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`
-  };
-  
-  request.get(summonerEndpointRequest, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const jsonData = JSON.parse(body);
-      summonerStats.name = jsonData.name;
-      summonerStats.id = jsonData.id;
-      res.json(jsonData)
-    } else {
-      console.log("Error making request to Summoner-V3.");
-    }
-  });
-
-  /*
     league/v3/positions/by-summoner/{summonerId}
   */
+
+ const options = {
+  uri: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.summonerName}`,
+  qs: {
+    api_key: 'RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55'
+  },
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true
+}
+
+requestPromise(options)
+  .then(function (response) {
+    summonerStats.name = response.name;
+    summonerStats.id = response.id;
+    res.json(summonerStats)
+    console.log("FIRST")
+  })
+  .then(function () {
+    console.log("AFTER")
+  })
+  .catch(function (error) {
+    console.log("Error making request to Summoner-V3.");
+  });
   
-  const leagueEndpointRequest = {
-    url: `https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/${summonerStats.id}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`
-  };
+  // const leagueEndpointRequest = {
+  //   url: `https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/${summonerStats.id}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`
+  // };
 
   //make sure the call to Summoner-V3 returned status 200
-  if (summonerStats.id !== undefined) {
-    request.get(leagueEndpointRequest, (error, response, body) => {
-      const jsonData = JSON.parse(body);
-      res.json(jsonData);
-    });
-  } else {
-    console.log("Can't call League-V3 because error making request to Summoner-V3.");
-  }
+  // if (summonerStats.id !== undefined) {
+  //   request.get(leagueEndpointRequest, (error, response, body) => {
+  //     const jsonData = JSON.parse(body);
+  //     res.json(jsonData);
+  //   });
+  // } else {
+  //   console.log("Can't call League-V3 because error making request to Summoner-V3.");
+  // }
 });   
 
 app.listen(port, () => {
