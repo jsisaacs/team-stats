9199083f5aac460b77ac5b6837cc16e7f8fe6424
Josh Isaacson <jsisaacs@iu.edu>
2018-05-31 01:11:07 -0400

    Created the summoner-stats endpoint. The requests need to be wrapped in Promises

--- server.js
+++ server.js
@@ -7,26 +7,61 @@ const port = process.env.PORT || 12344;
 const publicPath = path.join(__dirname, ".", "public");
 
 const app = express();
-
 app.use(cors());
-app.get("/", (req, res) => {
-  res.send("bullshit");
-});
-app.get("/summoner/:summonerName", (req, res) => {
-  console.log("got the request");
-  const summonerName = req.params.summonerName;
-  const data = {
-    url: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`,
-  };
-  request.get(data, (error, response, body) => {
-    res.send(body);
-  });
-});
+
+/*
+  Endpoint to recieve basic statistics about a summoner.
+*/
 
 app.get("/summoner-stats/:summonerName", (req, res) => {
-  const summonerName = req.params.summonerName;
   
-  res.send(summonerName)
+  //JSON to be returned
+  const summonerStats = {
+    name: undefined,
+    id: undefined,
+    tier: undefined,
+    rank: undefined,
+    leaguePoints: undefined,
+    winrate: undefined,
+    hotStreak: undefined,
+  };
+  
+  /*
+    summoner/v3/summoners/by-name/{summonerName}
+  */
+
+  const summonerEndpointRequest = {
+    url: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.summonerName}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`
+  };
+  
+  request.get(summonerEndpointRequest, (error, response, body) => {
+    if (response.statusCode === 200) {
+      const jsonData = JSON.parse(body);
+      summonerStats.name = jsonData.name;
+      summonerStats.id = jsonData.id;
+      res.json(jsonData)
+    } else {
+      console.log("Error making request to Summoner-V3.");
+    }
+  });
+
+  /*
+    league/v3/positions/by-summoner/{summonerId}
+  */
+  
+  const leagueEndpointRequest = {
+    url: `https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/${summonerStats.id}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`
+  };
+
+  //make sure the call to Summoner-V3 returned status 200
+  if (summonerStats.id !== undefined) {
+    request.get(leagueEndpointRequest, (error, response, body) => {
+      const jsonData = JSON.parse(body);
+      res.json(jsonData);
+    });
+  } else {
+    console.log("Can't call League-V3 because error making request to Summoner-V3.");
+  }
 });   
 
 app.listen(port, () => {
