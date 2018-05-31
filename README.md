b3d933242354771835c02ae756d7b1c529fb56d6
Josh Isaacson <jsisaacs@iu.edu>
2018-05-30 21:58:11 -0400

    added todo to the README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# League of legend 
-# Dodge this game or not ?
+# Team Statistics
+
+## TODO
+### Backend
+#### Endpoints
+- [] summoner-stats
+    - [] summoner name
+    - [] summoner id
+    - [] rank
+    - [] division
+    - [] win rate (last 20 games?)
+    - [] streak
+        - [] hot, cold, none (maybe more kinds)
+- [] summoner-score
+    - [] score (projected score based off of your play stats)
+    - [] strengths 
+- [] current-champion
+    - [] champion
+    - [] champion image
+    - [] champion mastery
+    - [] champion win rate
+    - [] kill participation (might be impossible without aggregation)
+- [] team-stats
+    - [] score
+    - [] strengths
+
+### Frontend
--- server.js
+++ server.js
@@ -5,23 +5,29 @@ const queryString = require("query-string");
 const path = require("path");
 const port = process.env.PORT || 12344;
 const publicPath = path.join(__dirname, ".", "public");
+
 const app = express();
+
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("bullshit");
 });
-app.get("/summoner/:username", (req, res) => {
+app.get("/summoner/:summonerName", (req, res) => {
   console.log("got the request");
-  const username = req.params.username;
+  const summonerName = req.params.summonerName;
   const data = {
-    url: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${username}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`,
+    url: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=RGAPI-05fc1dea-5fcc-40d3-b7c1-0e3fe69a8b55`,
   };
   request.get(data, (error, response, body) => {
     res.send(body);
   });
 });
 
-
+app.get("/summoner-stats/:summonerName", (req, res) => {
+  const summonerName = req.params.summonerName;
+  
+  res.send(summonerName)
+});   
 
 app.listen(port, () => {
   console.log("running at http://localhost:" + port);
