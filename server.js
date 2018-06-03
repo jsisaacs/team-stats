6361449e6708f5a3e29fa8533887aa55ad318275
Tianchi Xu <xtc459924276@gmail.com>
2018-06-03 13:44:31 -0400

    Get the solo queue info instead of flex queue

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const rp = require('request-promise');
-const cors = require("cors");
-const queryString = require("query-string");
-const path = require("path");
-const port = process.env.PORT || 12344;
-const publicPath = path.join(__dirname, ".", "public");
+const express = require("express")
+const rp = require('request-promise')
+const cors = require("cors")
+const queryString = require("query-string")
+const path = require("path")
+const port = process.env.PORT || 12344
+const publicPath = path.join(__dirname, ".", "public")
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 
 /*
   Endpoint to recieve basic statistics about a summoner.
@@ -24,7 +24,7 @@ app.get("/summoner-stats/:summonerName", (req, res) => {
     leaguePoints: undefined,
     winrate: undefined,
     hotStreak: undefined,
-  };
+  }
 
   /*
     summoner/v3/summoners/by-name/{summonerName}
@@ -33,7 +33,7 @@ app.get("/summoner-stats/:summonerName", (req, res) => {
   const summonerRequestOptions = {
     uri: `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.summonerName}`,
     qs: {
-      api_key: 'RGAPI-7b26bf63-d297-4e54-b54d-bad9081f99f0'
+      api_key: 'RGAPI-f7a35839-2c3d-4b01-9e00-05560d63edd4'
     },
     headers: {
       'User-Agent': 'Request-Promise'
@@ -49,7 +49,7 @@ app.get("/summoner-stats/:summonerName", (req, res) => {
     {
       uri: `https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/${id}`,
       qs: {
-        api_key: 'RGAPI-7b26bf63-d297-4e54-b54d-bad9081f99f0'
+        api_key: 'RGAPI-f7a35839-2c3d-4b01-9e00-05560d63edd4'
       },
       headers: {
         'User-Agent': 'Request-Promise'
@@ -60,22 +60,23 @@ app.get("/summoner-stats/:summonerName", (req, res) => {
 
   rp(summonerRequestOptions)
     .then(response => {
-      summonerStats.name = response.name;
-      summonerStats.id = response.id;
+      summonerStats.name = response.name
+      summonerStats.id = response.id
       rp(leagueRequestOptions(summonerStats.id))
         .then(response => {
-          summonerStats.tier = response[0].tier;
-          summonerStats.rank = response[0].rank;
-          summonerStats.leaguePoints = response[0].leaguePoints;
-          summonerStats.hotStreak = response[0].hotStreak;
-          res.json(summonerStats);
+          //Get the solo Queue info
+          const soloQueue = response.filter(league => league.queueType === 'RANKED_SOLO_5x5')[0]
+
+          summonerStats.tier = soloQueue.tier
+          summonerStats.rank = soloQueue.rank
+          summonerStats.leaguePoints = soloQueue.leaguePoints
+          summonerStats.hotStreak = soloQueue.hotStreak
+          res.json(summonerStats)
         })
         .catch(error => console.log(error.statusCode))
     })
-    .catch(error => {
-      console.log(error.statusCode)
-    });
-});
+    .catch(error => { console.log(error.statusCode) })
+})
 
 /*
   Endpoint about a summoner's current champion
@@ -83,9 +84,9 @@ app.get("/summoner-stats/:summonerName", (req, res) => {
 
 app.get("/current-champion/:summonerName", (req, res) => {
 
-});
+})
 
 
 app.listen(port, () => {
-  console.log("running at http://localhost:" + port);
-});
+  console.log("running at http://localhost:" + port)
+})
