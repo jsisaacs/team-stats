8069efd61581d955ef1bf8c71bc47add1409e036
Josh Isaacson <jsisaacs@iu.edu>
2018-06-13 17:59:22 -0400

    scraped the 20 most played champions and their respective game stats from OP.GG. Built an endpoint that returns an array of objects of each champ.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 const _kayn = require('kayn')
 const Kayn = _kayn.Kayn
 const REGIONS = _kayn.REGIONS
+const cheerio = require('cheerio')
 const path = require("path")
 const port = process.env.PORT || 12344
 const publicPath = path.join(__dirname, ".", "public")
@@ -29,10 +30,103 @@ const kayn = Kayn(process.env.API_KEY)({
 })
 
 /*
+  Returns a JSON object of the 20 most played champions and their game statistics, scraped from OP.GG via an AJAX request.
+ */
+
+app.get("/test/:summonerName", (req, res) => {
+  const summonerStats = [];
+  
+  const summonerStatsRequest = (summonerName) => (
+    summonerStatsRequestOptions = {
+      uri: `http://localhost:12344/summoner-stats/${summonerName}`,
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    }
+  );
+  
+  const opggChampionStats = (summonerId) => (
+    opGGRequestOptions = {
+      uri: `http://na.op.gg/summoner/champions/ajax/champions.rank/summonerId=${summonerId}&season=11&queueType=soloranked`,
+      transform: body => {
+        return cheerio.load(body);
+      }
+    }
+  );
+
+  rp(summonerStatsRequest(req.params.summonerName))
+    .then(response => {
+      rp(opggChampionStats(response.id))
+        .then($ => {
+          const tableData = $('.Body').html();
+          $('tr').each(function(i, elem) {
+            if (i > 20) {
+              return false;
+            } else {
+              const championData = {
+                championRank: i + 1,
+                championName: $('tr').not($('tr')[0]).eq(i).children().eq(1).children().first().children().text(),
+                championImage: undefined,
+                wins: Number($('tr').not($('tr')[0]).eq(i).children().eq(3).children().first().children().first().children().eq(1).text().slice(0, -1).replace(/,/g, '')),
+                losses: Number($('tr').not($('tr')[0]).eq(i).children().eq(3).children().first().children().first().children().eq(3).text().slice(0, -1).replace(/,/g, '')),
+                kda: {
+                  kills: Number($('tr').not($('tr')[0]).eq(i).children().eq(4).children().first().children().first().text()),
+                  deaths: Number($('tr').not($('tr')[0]).eq(i).children().eq(4).children().first().children().eq(1).text()),
+                  assists: Number($('tr').not($('tr')[0]).eq(i).children().eq(4).children().first().children().eq(2).text())
+                },
+                gold: Number($('tr').not($('tr')[0]).eq(i).children().eq(5).text().slice(6, -5).replace(/,/g, '')),
+                cs: Number($('tr').not($('tr')[0]).eq(i).children().eq(6).text().slice(6, -5).replace(/,/g, '')),
+                maxKills: Number($('tr').not($('tr')[0]).eq(i).children().eq(7).text().slice(7, -6).replace(/,/g, '')),
+                maxDeaths: Number($('tr').not($('tr')[0]).eq(i).children().eq(8).text().slice(7, -6).replace(/,/g, '')),
+                averageDamageDealt: Number($('tr').not($('tr')[0]).eq(i).children().eq(9).text().slice(7, -6).replace(/,/g, '')),
+                averageDamageTaken: Number($('tr').not($('tr')[0]).eq(i).children().eq(10).text().slice(7, -6).replace(/,/g, '')),
+                doubleKill: Number($('tr').not($('tr')[0]).eq(i).children().eq(11).text().slice(14, -12).replace(/,/g, '')),
+                tripleKill: Number($('tr').not($('tr')[0]).eq(i).children().eq(12).text().slice(14, -12).replace(/,/g, '')),
+                quadraKill: Number($('tr').not($('tr')[0]).eq(i).children().eq(13).text().slice(14, -12).replace(/,/g, '')),
+                pentaKill: Number($('tr').not($('tr')[0]).eq(i).children().eq(14).text().slice(14, -12).replace(/,/g, ''))
+              }
+              summonerStats.push(championData);
+          }
+          });
+          summonerStats.pop();
+          res.json(summonerStats);
+        })
+        .catch(error => {
+          console.log(`${error.statusCode}: Error accessing OP.GG data.`)
+        });
+    })
+    .catch(error => {
+      console.log(`${error.statusCode}: Error accessing summoner info.`)
+    });
+});
+
+/*
   Endpoint aggregating match data for a given summoner name and champion id.
 */
 
 app.get("/match-aggregations/:summonerName/:championId", (req, res) => {
+  matchAggregations = {
+    winRate: -1,
+  }
+
+  function getTeam(champId, match) {
+    match.participants.map(participant => {
+      if (participant.championId === champId) {
+        return participant.teamId;
+      }
+    })
+  }
+
+  function getWinRate(matchData) {
+    let wins = 0;
+    let losses = 0;
+    matchData.map(match => {
+      const team = getTeam(req.params.championId, match);
+      console.log(team);
+    })
+  }
+  
   kayn.Summoner.by
     .name(req.params.summonerName)
     .then(response => {
@@ -56,7 +150,8 @@ app.get("/match-aggregations/:summonerName/:championId", (req, res) => {
              
           Promise.all(matchDataRequests)
             .then(matchData => {
-              res.json(matchData);
+              //matchAggregations.winRate = getWinRate(matchData);
+              res.json("WOW");
             })
         })
         .catch(error => {
