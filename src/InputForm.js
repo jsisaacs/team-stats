4f0952e0276d0106450e1d7b5383b75c6a41b3dd
Tianchi Xu <xtc459924276@gmail.com>
2018-06-14 15:29:29 +0800

    show most played 10 champion images

--- src/InputForm.js
+++ src/InputForm.js
@@ -16,40 +16,39 @@ class InputForm extends React.Component {
     });
   };
 
-
   handleSubmit = event => {
     event.preventDefault();
     const text = this.state.value;
-    let splitText = text.replace(/ joined the lobby/g, '').split('\n')
-    console.log(splitText)
-
+    let splitText = text.replace(/ joined the lobby/g, "").split("\n");
     //add all summoners to the team
-    this.setState({ team: [] })
+    this.setState({ team: [] });
     for (let i = 0; i < splitText.length; i++) {
       fetch(`http://localhost:12344/summoner-stats/${splitText[i]}`)
         .then(res => res.json())
         .then(data => {
-          let team = [...this.state.team]
-          team.push(data)
-          this.setState({ team })
-        })
+          if (data.id !== -1) {
+            fetch(`http://localhost:12344/mostPlayed/${data.id}`).then(res => res.json()).then(mostPlayed => {
+              data.mostPlayed = mostPlayed
+              let team = [...this.state.team];
+              team.push(data);
+              this.setState({ team });
+            })
+          }
+        });
     }
   };
 
-  checkLiveGame = event => {
-
-  }
+  checkLiveGame = event => { };
 
   render() {
     return (
       <div>
-
         <form onSubmit={this.handleSubmit}>
           <label>
             Summoners:
             <textarea
-              rows='5'
-              cols='40'
+              rows="5"
+              cols="40"
               value={this.state.value}
               onChange={this.handleChange}
             />
@@ -59,13 +58,18 @@ class InputForm extends React.Component {
 
         <ul>
           {this.state.team.map(user => (
-            <div>
-              <li key={user.id}>{`Name: ${user.name}`}</li>
+            <div key={user.id}>
+              <li>{`Name: ${user.name}`}</li>
+              <li>{`Rank: ${user.tier} ${user.rank}`}</li>
+              <li>Most played champion:</li>
+              {user.mostPlayed.map(d => {
+                return <img key={d.championName} src={`http://opgg-static.akamaized.net/images/lol/champion/${d.championName.replace(/ |'/g, '')}.png?image=w_50`} alt={d.championName} />
+              })}
+              <br />
               <button onClick={this.checkLiveGame}>Live game</button>
             </div>
           ))}
-        </ul>
-
+        </ul>›
       </div>
     );
   }
