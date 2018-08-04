cec031e3813fb3031245e4ff4feb537b510377b1
Josh Isaacson <jsisaacs@iu.edu>
2018-08-04 12:45:16 -0400

    CSS for Modal, Content, and CloseButton components.

--- src/ExpandedTeammate.js
+++ src/ExpandedTeammate.js
@@ -2,9 +2,39 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import rp from 'request-promise';
 
-const Modal = styled.div``;
-const Content = styled.div``;
-const CloseButton = styled.span``;
+const Modal = styled.div`
+  display: block;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0,0.4);
+  backdrop-filter: blur(200px);
+`;
+const Content = styled.div`
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+`;
+const CloseButton = styled.span`
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+
+  &:hover {
+    color: #000;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
 
 class ExpandedTeammate extends Component {
   constructor(props) {
